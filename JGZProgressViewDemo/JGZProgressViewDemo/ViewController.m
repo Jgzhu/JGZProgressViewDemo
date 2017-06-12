4cb6d0b130110d43750a0df86f4ed7348be15fe3
jgzhu <251475337@qq.com>
2017-06-12 17:32:47 +0800

    初始化项目

---
+++ JGZProgressViewDemo/JGZProgressViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JGZProgressViewDemo
+//
+//  Created by 江贵铸 on 2017/6/12.
+//  Copyright © 2017年 江贵铸. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ JGZProgressViewDemo/JGZProgressViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JGZProgressViewDemo
+//
+//  Created by 江贵铸 on 2017/6/12.
+//  Copyright © 2017年 江贵铸. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
